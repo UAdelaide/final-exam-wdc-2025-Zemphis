e51e4c45e95e8329d4b4d839f8dcfc6f1562757b
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 10:59:15 +0000

    [automatic commit] 2025-06-20 10:59:15 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -43,7 +43,7 @@ async function insertInitialData() {
             console.log('Database tables cleared');
 
             await connection.execute(
-                ''
+                'INSERT INTO USERS (user_id, email, password, role) VALUES ' +
             )
         }
     }
