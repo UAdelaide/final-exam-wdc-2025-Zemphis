60e443a4c7dc23ee81f2265e6b389eb2b33227d3
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 11:13:01 +0000

    [automatic commit] 2025-06-20 11:13:01 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -65,7 +65,7 @@ async function insertInitialData() {
             console.log('Initial data inserted');
 
             await connection.execute(
-                'INSERT INTO '
+                'INSERT INTO Dogs (owner_id, name'
             );
     }
 }
