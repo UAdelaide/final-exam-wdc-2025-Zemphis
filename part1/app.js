9f1eb181859fdf9891746ef67d03465e48d7dd18
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 11:38:10 +0000

    [automatic commit] 2025-06-20 11:38:09 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -94,6 +94,10 @@ async function insertInitialData() {
                 'INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (?, ?, ?, ?, ?)',
                 [2, '2025-06-11 09:30:00', 45, 'Beachside Ave', 'accepted']
             );
+            await connection.execute(
+                'INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (?, ?, ?, ?, ?)',
+                [3, '2025-06-12 10:15:00', 60, 'Downtown Park', 'open']
+            );
         }
 
     }
