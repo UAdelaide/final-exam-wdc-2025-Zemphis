b873971d795c01a879f3c2c7e874f1c551db2029
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 19:43:15 +0000

    [automatic commit] 2025-06-20 19:43:14 UTC 115.131.80.152

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -222,6 +222,8 @@ function login() {
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this)
+        if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(this.responseText);
+        }
     }
 }
