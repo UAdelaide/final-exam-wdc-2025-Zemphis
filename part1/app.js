b8ec0efbe9e33cb94b42b0cbadc555a0c75bcdbc
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 11:19:45 +0000

    [automatic commit] 2025-06-20 11:19:44 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -87,7 +87,7 @@ async function insertInitialData() {
             console.log('Initial dog data inserted');
 
             await connection.execute(
-                
+                'INSERT INTO WalkRequests '
             );
 
     }
