b13cbd41595e3eaba64d4c3705c1d945683b6780
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 11:35:42 +0000

    [automatic commit] 2025-06-20 11:35:42 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -95,6 +95,7 @@ async function insertInitialData() {
                 'INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (?, ?, ?, ?, ?)',
                 [2, '2025-06-11 09:30:00', 45, 'Beachside Ave', 'accepted']
             );
+        }
 
     }
 }
