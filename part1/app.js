6683c3fcd17d185a962be0c11bcf261d63501598
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 13:01:19 +0000

    [automatic commit] 2025-06-20 13:01:18 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -218,5 +218,6 @@ async function startServer() {
 
     app.listen(3000, () => {
         console.log('Server is running on http://localhost:3000');
+        console.log('Test the API endpoints:');
     }
 }
