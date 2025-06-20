6d77f5c7de8a0915aca9dfb00c61b45f75547726
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 13:17:09 +0000

    [automatic commit] 2025-06-20 13:17:09 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-var bodyParser = require('body-parser');
-var app = express();
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
