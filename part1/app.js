6760ad15fcba695203f2ca96ba0250d8dc4677a2
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 12:53:00 +0000

    [automatic commit] 2025-06-20 12:52:59 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -182,7 +182,8 @@ app.get('/api/walkers/summary', async (req, res) => {
                 WHERE
                     wa.walker_id = ? AND wr.status = 'accepted' AND wa.status = 'completed';
             `, [walker_id]);
-            const completed_walks = compltedWalks[0].completed_walks || 0
+            const completed_walks = compltedWalks[0].completed_walks || 0;
+            
         }
     }
 });
