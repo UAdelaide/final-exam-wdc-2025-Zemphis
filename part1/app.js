2e4c09bc9e5dbccc4a0a048357df93a977924542
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 12:20:40 +0000

    [automatic commit] 2025-06-20 12:20:39 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -131,6 +131,7 @@ app.get('/api/dogs', async (req, res) => {
             d.name AS dog_name,
             d.size,
             u.username AS owner_username
-            F);
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.id);
     }
 });
