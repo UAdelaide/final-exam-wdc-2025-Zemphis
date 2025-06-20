80ef38c57e949a20b0c82e96428fa7fb3d3b82aa
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 13:44:39 +0000

    [automatic commit] 2025-06-20 13:44:39 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -65,7 +65,8 @@ async function insertInitialData() {
             );
             console.log('Initial data inserted');
 
-            const []
+            const [alice] = await connection.execute('SELECT id FROM Users WHERE username = ?', ['alice123']);
+            
 
             await connection.execute(
                 'INSERT INTO Dogs (owner_id, name, size) VALUES (?, ?, ?)',
