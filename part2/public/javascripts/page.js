bb66489ac64cac244cedbe119c459ca4a02b1ff0
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 20:07:17 +0000

    [automatic commit] 2025-06-20 20:07:16 UTC 115.131.80.152

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -206,7 +206,9 @@ function login() {
             alert("login failed");
         }
 
-        x
+        xmlhttp.open("POST", "/users/login", true);
+        xmlhttp.setRequestHeader("Content-type", "application/json");
+        xmlhttp.send(JSON.stringify(user));
     };
 
     xmlhttp.open("POST", "/users/login", true);
