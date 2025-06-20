78574146cfb4dd4a7e9467fd9080a2c617d5c1dc
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 21:08:53 +0000

    [automatic commit] 2025-06-20 21:08:53 UTC 115.131.80.152

--- part2/app.js
+++ part2/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2/promise'); // Using mysql2/promise for async/await support
+
 const path = require('path');
 require('dotenv').config();
 
