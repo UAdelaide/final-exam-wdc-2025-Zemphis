93c625e225d1fda60f4cbdd226d6b64e0ad5b06e
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 12:38:29 +0000

    [automatic commit] 2025-06-20 12:38:28 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -166,5 +166,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/walkers/summary', async (req, res) => {
     try {
         const [walkers] = await pool.execute("SELECT user_id, username FROM Users WHERE role = 'walker'");
+        const summary = [];
+        
     }
 });
