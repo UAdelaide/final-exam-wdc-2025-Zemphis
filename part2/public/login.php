e3a6d5176006ebf773804f4162bf225149d63eec
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 18:57:01 +0000

    [automatic commit] 2025-06-20 18:57:01 UTC 115.131.80.152

--- part2/public/login.php
+++ part2/public/login.php
@@ -10,4 +10,8 @@ if ($conn->connect_error) {
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+// Prepare and bind
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
