fe7ebcd7bc80a4d9c8344393c1d88345caf557fd
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 13:22:52 +0000

    [automatic commit] 2025-06-20 13:22:51 UTC 115.131.80.152

--- part1/app.js
+++ part1/app.js
@@ -220,7 +220,7 @@ async function startApp() {
     app.listen(port, () => {
         console.log('Server is running on http://localhost:3000');
         console.log('Test the API endpoints:');
-        console.log('- http://localhost:3000/api/dogs');
+        console.log('- http://localhost:${port}/api/dogs');
         console.log('- http://localhost:3000/api/walkrequests/open');
         console.log('- http://localhost:3000/api/walkers/summary');
     });
