e23a136500962fcf8df47bd3c3c8c535ce234b9f
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 18:55:36 +0000

    [automatic commit] 2025-06-20 18:55:35 UTC 115.131.80.152

--- part2/public/login.php
+++ part2/public/login.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
 
-$conn = new mysqli("localhost", "root", "", "DogWalkService");
+$conn = new mysqli("localhost", "root", "", "DogWalkService"); // Update with database credentials
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
