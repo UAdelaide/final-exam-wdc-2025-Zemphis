dd715542bdfbefd36e01189b88d710048529e381
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-20 20:46:34 +0000

    [automatic commit] 2025-06-20 20:46:33 UTC 115.131.80.152

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -193,7 +193,7 @@ function login() {
                     } else if (response.role === 'walker') {
                         window.location.href = 'walker-dashboard.html';
                     } else {
-                        alert('Login successful, but no valid role found.');
+                        alert('Login succes
                     }
                 }
             }
