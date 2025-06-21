8817dc73e0430830996df5c0a5065cd7f006707a
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-21 05:33:47 +0000

    [automatic commit] 2025-06-21 05:33:46 UTC 115.131.80.152

--- part2/app.js
+++ part2/app.js
@@ -26,7 +26,9 @@ app.use('/api/users', userRoutes);
 
 //Redirect
 app.get('/owner', (req, res) => {
-    if (req.session.user && req.session.user.role === '')
+    if (req.session.user && req.session.user.role === 'owner') {
+        res.sendFile(path,)
+    }
 });
 
 
