7f0dccd508631cce5e5ea0911bcfa9ec77e1934e
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-21 05:30:36 +0000

    [automatic commit] 2025-06-21 05:30:36 UTC 115.131.80.152

--- part2/app.js
+++ part2/app.js
@@ -12,7 +12,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(session({
     secret: 'test',
-    resave: false
+    resave: false,
+    saveUninitialized: false
 }));
 
 // Routes
