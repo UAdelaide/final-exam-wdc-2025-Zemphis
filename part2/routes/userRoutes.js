fe4f0b33e116c648a2bae901ac666bbf1dfd8998
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-21 05:39:24 +0000

    [automatic commit] 2025-06-21 05:39:24 UTC 115.131.80.152

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,9 @@ router.get('/me', (req, res) => {
 
 // POST login
 router.post('/login', async(req, res) => {
-  db.get(db.query, [username], async (err, user) => {})
-})
+  db.get(db.query, [username], async (err, user) => {
+    
+  });
+});
 
 module.exports = router;
