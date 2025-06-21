983ea2aa5d65b4c44a33053e0fee38d2126af1be
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-21 04:57:13 +0000

    [automatic commit] 2025-06-21 04:57:13 UTC 115.131.80.152

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const cookiePars
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
 const db = require('./models/db'); // Import the database pool
