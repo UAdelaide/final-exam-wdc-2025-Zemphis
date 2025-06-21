f6cd89890e8588766e83bad6a4803af936bb8464
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-21 05:44:00 +0000

    [automatic commit] 2025-06-21 05:44:00 UTC 115.131.80.152

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async(req, res) => {
   const query = 'SELECT * FROM Users WHERE username = ?';
   db.get(db.query, [username], async (err, user) => {
     if (err) return res.status(500).send('Server Error');
-    
+    if (!user) return res.status(401).send('')
   });
 });
 
